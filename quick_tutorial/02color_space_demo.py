a8d6be5d11dad4ecda98173775ab8a207c2a2281
zccbbg <zccbbg@qq.com>
2023-07-09 13:33:46 +0800

    增加一些备注

--- quick_tutorial/02color_space_demo.py
+++ quick_tutorial/02color_space_demo.py
@@ -3,6 +3,13 @@ import numpy as np
 
 '''
 图像色彩空间转换
+HSV颜色模型
+HSV(Hue, Saturation, Value)是根据颜色的直观特性由A. R. Smith在1978年创建的一种颜色空间, 也称六角锥体模型(Hexcone Model)。
+这个模型中颜色的参数分别是：色调（H），饱和度（S），亮度（V）。
+色调H：用角度度量，取值范围为0°～360°，从红色开始按逆时针方向计算，红色为0°，绿色为120°,蓝色为240°。它们的补色是：黄色为60°，青色为180°,品红为300°；
+饱和度S：取值范围为0.0～1.0；
+亮度V：取值范围为0.0(黑色)～1.0(白色)。
+https://blog.csdn.net/qinglongzhan/article/details/107213767
 '''
 def color_space_demo():
     image = cv.imread("../images/test.jpg")
--- quick_tutorial/03mat_demo1.py
+++ quick_tutorial/03mat_demo1.py
@@ -2,6 +2,21 @@ import cv2 as cv
 import numpy as np
 '''
 图像对象的创建
+numpy.zeros_like：返回与指定数组具有相同形状和数据类型的数组，并且数组中的值都为0。
+>>> x = np.arange(6)
+>>> x = x.reshape((2, 3))
+>>> x
+array([[0, 1, 2],
+[3, 4, 5]])
+>>> np.zeros_like(x)
+array([[0, 0, 0],
+[0, 0, 0]])
+>>>
+>>> y = np.arange(3, dtype=float)
+>>> y
+array([ 0., 1., 2.])
+>>> np.zeros_like(y)
+array([ 0., 0., 0.])
 '''
 def mat_demo1():
     image = cv.imread("../images/test.jpg")
