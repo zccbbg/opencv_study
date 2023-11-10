a1144f95cb61239e1b960b5aaef99c14ffdf4e54
zccbbg <zccbbg@qq.com>
2023-11-10 18:57:38 +0800

    更新注释

--- quick_tutorial/09color_table_demo.py
+++ quick_tutorial/09color_table_demo.py
@@ -5,6 +5,10 @@ import numpy as np
 '''
 
 def demo():
+    '''
+    定义了一个列表colormap，其中包含了多种颜色映射的选项，通过在图像上应用不同的颜色映射，可以改变图像的色调和外观。
+    共有19种不同的颜色映射选项，可以在cv.applyColorMap函数中使用。
+    '''
     colormap = [
         cv.COLORMAP_AUTUMN,
         cv.COLORMAP_BONE,
@@ -31,10 +35,14 @@ def demo():
     cv.imshow("input", image)
     index=0
     while True:
+        '''
+        使用cv.applyColorMap函数将当前选定的颜色映射应用到原始图像上。
+        colormap[index % 19]选择颜色映射列表中的一个颜色映射，index % 19确保索引循环在0到18之间，以便循环应用不同的颜色映射。
+        '''
         dst = cv.applyColorMap(image,colormap[index%19])
         index+=1
         cv.imshow("color style",dst)
-        c=cv.waitKey(200)
+        c=cv.waitKey(200) # 等待200毫秒
         if c==27:
             break
     cv.destroyAllWindows()
