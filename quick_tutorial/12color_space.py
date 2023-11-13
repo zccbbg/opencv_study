07ed185072fecb123e1130e07497dce14160c716
zccbbg <zccbbg@qq.com>
2023-11-13 18:02:47 +0800

    更新注释

--- quick_tutorial/12color_space.py
+++ quick_tutorial/12color_space.py
@@ -16,11 +16,27 @@ def demo():
     b1 = cv.imread("../images/greenback.png")
     print(b1.shape)
     cv.imshow("input",b1)
-
+    '''
+    使用cv.cvtColor函数将原始图像b1从BGR颜色空间转换为HSV颜色空间
+    '''
     hsv = cv.cvtColor(b1,cv.COLOR_BGR2HSV)
     cv.imshow("hsv",hsv)
+    '''
+    使用cv.inRange函数生成一个二值掩码mask，
+    HSV图像的像素值在指定范围内的被设为255（白色），不在范围内的被设为0（黑色）。
+    在这里，指定的HSV范围为(35, 43, 46)到(77, 255, 255)，这个范围用于表示绿色的颜色范围。
+    '''
     mask = cv.inRange(hsv,(35,43,46),(77,255,255))
+    '''
+    使用cv.bitwise_not函数对掩码取反，将白色变为黑色，黑色变为白色
+    这样，生成的掩码将包含图像中非绿色区域的白色像素
+    '''
     cv.bitwise_not(mask,mask)
+    '''
+    使用cv.bitwise_and函数对原始图像b1和生成的掩码mask进行按位与操作。
+    这意味着只有在掩码为白色（255）的位置，原始图像的对应位置的像素值才被保留，其他位置的像素值为0。
+    最终生成的result图像将只保留原图中绿色区域的像素，而非绿色区域的像素被设为0。
+    '''
     result = cv.bitwise_and(b1,b1,mask=mask)
     cv.imshow("mask",mask)
     cv.imshow("result",result)
